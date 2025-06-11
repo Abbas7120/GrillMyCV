799a9f9685c0e3004f6eb564b5dffc59bca7b750
Abbas <abbasfzd118@gmail.com>
2025-06-12 03:52:08 +0530

    Fix: Proper uploads folder structure with .gitkeep

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,16 @@ const express=require('express')
 const resumeRoutes=require("./routes/resumeRoutes")
 const app=express()
 const cors=require('cors')
+const fs=require('fs')
+const path=require('path')
 //routes ->left
 app.use(cors())
 app.use(express.json())
 app.use("/api/resume",resumeRoutes)
+const uploadDir=path.join(__dirname,'uploads')
+if(!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir)
+
+}
 const PORT=5000
 app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`)})
